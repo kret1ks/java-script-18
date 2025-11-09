7fc2c0f2060a14d4d3c27bfb6f1e7e6a29232edd
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-09 13:07:01 +0200

    123

---
+++ js/task-1.js
@@ -0,0 +1,55 @@
+const categoriesRef = document.querySelector('[id="categories"]')
+const firstItem = categoriesRef.querySelector('.item')
+
+const heading = firstItem.querySelector('.h2')
+const categories = heading.textContent
+
+const elements = firstItem.querySelector
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const categoriesRef = document.querySelector('[id="categories"]');
+// const firstItem = categoriesRef.querySelector('.item');
+
+// const heading = firstItem.querySelector("h2");
+// const categoryName = heading.textContent;
+
+// const elements = firstItem.querySelectorAll("li");
+// const elementsCount = elements.length;
+
+// console.log(`Категорія: ${categoryName}`);
+// console.log(`Кількість елементів: ${elementsCount}`);

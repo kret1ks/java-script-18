4f84aea9df0e123a34358e3a77af3e1636a3708e
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-10 20:38:01 +0200

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,14 @@
-const categoriesRef = document.querySelector('[id="categories"]')
-const firstItem = categoriesRef.querySelector('.item')
-
-const heading = firstItem.querySelector('.h2')
-const categories = heading.textContent
-
-const elements = firstItem.querySelector
+// const listRef = document.querySelectorAll("#categories .item");
 
 
-
-
-
-
+// listRef.forEach((item) => {
+//     const titleRef = item.querySelector("h2");
+//     const itemRef = item.querySelectorAll("li")
+    
+    
+//     console.log(`Категорія: ${titleRef.textContent}`);
+//     console.log(`кількість елементів: ${itemRef.length}`);
+// });
 
 
 
@@ -24,32 +22,56 @@ const elements = firstItem.querySelector
 
 
 
+// const ingredients = [
+//   'Картопля',
+//   'Гриби',
+//   'Часник',
+//   'Помідори',
+//   'Зелень',
+//   'Приправи',
+// ];
+
+
+// // const ingredientsList = document.querySelector("#ingredients")
+
+// // ingredients.forEach((item) => {
+// //     const li = document.createElement('li')
+// //     li.textContent = item
+// //     listRef.append(li)
+// // })
+
+
+// const ingredientsList = document.querySelector("#ingredients")
+// const items = ingredients.map((ingredient) => {
+//     const li = document.createElement('li')
+//     li.textContent = ingredient;
+//     return li
+// })
+
+// ingredientsList.append(...items)
 
 
 
 
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const categoriesRef = document.querySelector('[id="categories"]');
-// const firstItem = categoriesRef.querySelector('.item');
-
-// const heading = firstItem.querySelector("h2");
-// const categoryName = heading.textContent;
-
-// const elements = firstItem.querySelectorAll("li");
-// const elementsCount = elements.length;
-
-// console.log(`Категорія: ${categoryName}`);
-// console.log(`Кількість елементів: ${elementsCount}`);
+const listRef = document.getElementById("gallery")
+const itemsWorkUp = images.map(({url, alt})=> `<li><img src="${url}" alt="${alt}"></li>`).join("")
+console.log(itemsWorkUp);
+listRef.insertAdjacentHTML("beforeend", itemsWorkUp)

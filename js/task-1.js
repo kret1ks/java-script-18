546fe67b0725ccd64f292c180f78101752246330
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-11 16:58:11 +0200

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,14 @@
-// const listRef = document.querySelectorAll("#categories .item");
+const listRef = document.querySelectorAll("#categories .item");
 
 
-// listRef.forEach((item) => {
-//     const titleRef = item.querySelector("h2");
-//     const itemRef = item.querySelectorAll("li")
+listRef.forEach((item) => {
+    const titleRef = item.querySelector("h2");
+    const itemRef = item.querySelectorAll("li")
     
     
-//     console.log(`Категорія: ${titleRef.textContent}`);
-//     console.log(`кількість елементів: ${itemRef.length}`);
-// });
+    console.log(`Категорія: ${titleRef.textContent}`);
+    console.log(`кількість елементів: ${itemRef.length}`);
+});
 
 
 
